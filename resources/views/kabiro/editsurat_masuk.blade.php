e8310f0ad11d297671acc933577dd5fa02262e6b
rizkarahayu <rizkarahayu42@gmail.com>
2020-05-19 16:46:47 +0700

    revisi ke3

--- resources/views/kabiro/editsurat_masuk.blade.php
+++ resources/views/kabiro/editsurat_masuk.blade.php
@@ -24,7 +24,7 @@
                         <div class="modal-body">
                             <div class="card card-default">
                                     <div class="card-header">
-                                        <h3 class="card-title">Detail Surat Masuk Jenis Pengadaan - Penunjukan Langsung</h3>
+                                        <h3 class="card-title">Detail Surat Masuk Jenis Pengadaan</h3>
                                         <div class="card-tools">
                                         <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
                                         <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
