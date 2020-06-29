872c08e36381a7d528fd324d4ea2f280ecbab333
rizkarahayu <rizkarahayu42@gmail.com>
2020-06-29 07:59:08 +0700

    sph upload2

---
+++ database/migrations/2020_06_29_005751_add_status_field_to_doksph_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddStatusFieldToDoksphTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('doksph', function (Blueprint $table) {
+            $table->string('status_surat')->after('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('doksph', function (Blueprint $table) {
+            //
+        });
+    }
+}
