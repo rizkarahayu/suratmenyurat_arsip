4a49704886d2b4ffc6d86663ad90bd7076e5a593
rizkarahayu <rizkarahayu42@gmail.com>
2020-07-24 21:52:55 +0700

    table surat_keluar

--- resources/views/sekretaris/datasurat_keluar.blade.php
+++ resources/views/sekretaris/datasurat_keluar.blade.php
@@ -29,7 +29,7 @@
                     @endif
             <div class="card card-default">
                 <div class="card-header">
-                    <h3 class="card-title">Tabel Data Surat Keluar Status Kepala Biro</h3>
+                    <h3 class="card-title">Tabel Data Surat Keluar Status Kepala Departemen</h3>
                     <div class="card-tools">
                         <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
                             <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
